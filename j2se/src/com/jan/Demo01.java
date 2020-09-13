843ade9055b3705318114ccc4432fa4ae03d0e7e
hongchen <hongchen998@foxmail.com>
2020-09-13 22:39:40 +0800

    first commit

---
+++ j2se/src/com/jan/Demo01.java
@@ -0,0 +1,14 @@
+package com.jan;/**
+ * @author hongchen
+ * @date 2020/9/13 10:51
+ */
+
+/**
+ *@ClassName Demo01
+ *@Description
+ *@Author Jan
+ *@Date 10:51 2020/9/13
+ *@Version 2.1
+ **/
+public class Demo01 {
+}
